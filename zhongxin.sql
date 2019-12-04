997a3f32542675b5a9c50748be8cf8a6f5f1c0cd
lxl <lxl_829@163.com>
2019-12-04 16:47:49 +0800

    数据库去除员工业绩，接口文档增加收益

--- zhongxin.sql
+++ zhongxin.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `username` varchar(50) NOT NULL COMMENT '用户名',
   `password` varchar(50) NOT NULL COMMENT '用户密码，MD5加密',
   `realname` varchar(50) DEFAULT  NULL COMMENT  '用户真实姓名',
-  `user_id` int(11) DEFAULT NULL COMMENT '用户身份证号码',
+  `id_card` int(20) DEFAULT NULL COMMENT '用户身份证号码',
   `phone` varchar(20) DEFAULT NULL,
-  `purchasedNum` decimal(20,2) NOT NULL COMMENT '用户购买总金额',
   `role` int(4) NOT NULL COMMENT '角色0-管理员,1-普通用户',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `product`;
@@ -30,32 +30,28 @@ CREATE TABLE `product` (
 
 DROP TABLE IF EXISTS `employee`;
 CREATE TABLE `employee` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '员工id',
+  `employee_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '员工id',
   `employeename` varchar(50) NOT NULL COMMENT '员工名',
-  `performance` decimal(20,2)  DEFAULT NULL COMMENT '员工业绩',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`employee_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '订单id',
+  `order_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '订单id',
   `order_no` bigint(20) DEFAULT NULL COMMENT '订单号',
-  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `user_id` int(11) NOT NULL COMMENT '用户id',
   `goods_id` int(11) NOT NULL COMMENT '产品id',
   `referrer_id` int(11) DEFAULT NULL COMMENT '推荐码',
   `payment` decimal(20,2) DEFAULT NULL COMMENT '实际付款金额,单位为元,保留两位小数',
   `payment_time` datetime DEFAULT NULL COMMENT '支付时间',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`order_id`),
   UNIQUE KEY `order_no_index` (`order_no`) USING BTREE,
-  constraint FK_user foreign key (username) references user(username) on delete cascade on update cascade,
+  constraint FK_user foreign key (user_id) references user(user_id) on delete cascade on update cascade,
   constraint FK_goods foreign key (goods_id) references product(goods_id) on delete cascade on update cascade,
-  constraint FK_employee foreign key (referrer_id) references employee(id) on delete cascade on update cascade
+  constraint FK_employee foreign key (referrer_id) references employee(employee_id) on delete cascade on update cascade
 ) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8;
-
-
-
 
